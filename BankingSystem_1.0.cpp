91de3456d5465ef0d371a81bfef75c5ab8e8dff3
Aditya Kasaudhan <72161687+aaditya173@users.noreply.github.com>
2024-03-21 10:19:34 +0530

    Add files via upload

---
+++ BankingSystem_1.0.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+using namespace std;
+
+static int accountNo = 0;
+
+struct Account
+{
+    string firstName, lastName;
+    int mobileNo, accountNumber, balance;
+} acc[500];
+
+void openAccount();
+void balanceEnquiry();
+void deposite();
+void withdrawal();
+void closeAccount();
+void showAllAccount();
+
+int main()
+{
+    int choice;
+    cout << "Banking Console System" << endl;
+    do
+    {
+        cout << "Select one option below" << endl;
+        cout << "1. Open Account" << endl;
+        cout << "2. Balance Enquiry" << endl;
+        cout << "3. Deposite" << endl;
+        cout << "4. Withdrawal" << endl;
+        cout << "5. Close an Account" << endl;
+        cout << "6. Show all Account" << endl;
+        cout << "7. Quit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            openAccount();
+            break;
+        case 2:
+            balanceEnquiry();
+            break;
+        case 3:
+            deposite();
+            break;
+        case 4:
+            withdrawal();
+            break;
+        case 5:
+            closeAccount();
+            break;
+        case 6:
+            showAllAccount();
+            break;
+        case 7:
+            cout << "Exit Successful" << endl;
+            break;
+        default:
+            cout << "Wrong Choice" << endl;
+            break;
+        }
+    } while (choice != 7);
+
+    return 0;
+}
+
+void openAccount()
+{
+    cout << "Enter First Name : ";
+    cin >> acc[accountNo].firstName;
+    cout << "Enter Last Name : ";
+    cin >> acc[accountNo].lastName;
+    cout << "Enter Mobile Number : ";
+    cin >> acc[accountNo].mobileNo;
+    acc[accountNo].accountNumber = accountNo;
+    acc[accountNo].balance = 0;
+    cout << "Account Created Successfully" << accountNo << endl;
+    accountNo++;
+}
+void balanceEnquiry()
+{
+    int ac;
+    cout << "Enter Account Number to check : ";
+    cin >> ac;
+    if (ac >= 0 && ac <= accountNo)
+    {
+        cout << "Balance is : " << acc[ac].balance;
+    }
+    else
+    {
+        cout << "Envalid Account Number" << endl;
+        return;
+    }
+}
+void deposite()
+{
+    int ac, deposite;
+    cout << "Enter the Account Number to deposite : ";
+    cin >> ac;
+    if (ac >= 0 && ac <= accountNo)
+    {
+        cout << "Enter the amount to deposite : ";
+        cin >> deposite;
+        acc[ac].balance += deposite;
+        cout << "Deposite of " << deposite << " is successfull" << endl;
+    }
+    else
+    {
+        cout << "Envalid Account Number" << endl;
+        return;
+    }
+}
+void withdrawal()
+{
+    int ac, withdrawal;
+    cout << "Enter the Account Number to withdrawal : ";
+    cin >> ac;
+    if (ac >= 0 && ac <= accountNo)
+    {
+        cout << "Enter the amount to withdrawal : ";
+        cin >> withdrawal;
+        acc[ac].balance -= withdrawal;
+        cout << "Withdrawal of " << withdrawal << " is successfull" << endl;
+    }
+    else
+    {
+        cout << "Envalid Account Number" << endl;
+        return;
+    }
+}
+void closeAccount()
+{
+    int ac;
+    cout << "Enter the Account Number to close : ";
+    cin >> ac;
+    if (ac >= 0 && ac <= accountNo)
+    {
+        acc[ac].firstName = acc[ac].lastName = "null";
+        acc[ac].mobileNo = acc[ac].balance = 0;
+        cout << "Account closed successfull" << endl;
+    }
+    else
+    {
+        cout << "Envalid Account Number" << endl;
+        return;
+    }
+}
+void showAllAccount()
+{
+    for (int i = 0; i < accountNo; i++)
+    {
+        cout << "Name is : " << acc[i].firstName << " " << acc[i].lastName << endl;
+        cout << "Account Number is : " << acc[i].accountNumber << endl;
+        cout << "Mobile Number is : " << acc[i].mobileNo << endl;
+    }
+}
